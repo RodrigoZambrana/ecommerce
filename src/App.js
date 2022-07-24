2f9c19b336ea085e198305aa8f6905aed81ad520
RodrigoZambrana <rodrigo@lacnic.net>
2022-07-24 11:15:44 -0300

    primer commit. Funciona login con password

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import Home from "./routes/home/home.component";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
+import Authentication from "./routes/authentication/authentication.component";
+
+const Shop = () => {
+  return (
+    <div>
+      <div>
+        <h1>Shop page</h1>
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index={true} element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="auth" element={<Authentication />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,76 @@
+import { initializeApp } from 'firebase/app'
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth'
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDDqs-Xm4BOhjDZStyyqu1N6acOF1zV_xg',
+  authDomain: 'urucortinas.firebaseapp.com',
+  projectId: 'urucortinas',
+  storageBucket: 'urucortinas.appspot.com',
+  messagingSenderId: '103220885950',
+  appId: '1:103220885950:web:7eb9f0d7b745a55e14e1ef',
+  measurementId: 'G-MWZ838MKZ9',
+}
+
+const firebaseApp = initializeApp(firebaseConfig)
+
+const googleProvider = new GoogleAuthProvider()
+
+googleProvider.setCustomParameters({
+  prompt: 'select_account',
+})
+
+export const auth = getAuth()
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider)
+
+export const db = getFirestore()
+
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {},
+) => {
+  if (!userAuth) return
+
+  const userDocRef = doc(db, 'users', userAuth.uid)
+
+  const userSnapshot = await getDoc(userDocRef)
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth
+    const createdAt = new Date()
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation,
+      })
+    } catch (error) {
+      console.log('error creating the user', error.message)
+    }
+  }
+
+  return userDocRef
+}
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return
+
+  return await createUserWithEmailAndPassword(auth, email, password)
+}
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return
+
+  return await signInWithEmailAndPassword(auth, email, password)
+}
