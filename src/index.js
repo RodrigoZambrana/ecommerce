794e644d17ac88e141e0f9f10495f4cf7980a8f4
RodrigoZambrana <rodrigo@lacnic.net>
2022-07-24 16:08:07 -0300

    funciona count de produtos del carrito

--- src/index.js
+++ src/index.js
@@ -5,16 +5,20 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { UserProvider } from './contexts/user.context'
+import { ProductsProvider } from './contexts/products.context'
+import { CartProvider } from './contexts/cart.context'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <App />
-      </UserProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <UserProvider>
+      <ProductsProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </BrowserRouter>,
 )
 
 // If you want to start measuring performance in your app, pass a function
